8b53b7d0b1bdf5c2434a1f5272193573bfb22201
Petr Posvic <petr@posvic.cz>
2016-04-17 16:15:08 +0200

    In game use US keyboard

--- run.sh
+++ run.sh
@@ -6,7 +6,9 @@ sudo mount diablo1.iso ~/mnt
 
 cd ~/.wine/drive_c/Diablo
 xrandr --output HDMI-0 --mode 800x600 --output VGA-0 --mode 1440x900 --below HDMI-0
+setxkbmap us
 wine diablo.exe
+setxkbmap cz
 xrandr --output HDMI-0 --mode 1920x1080 --output VGA-0 --mode 1440x900 --below HDMI-0
 
 sudo umount ~/mnt
